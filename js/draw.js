6cc265b432828061fc2fdadbc6225433d1cab659
Meredith Rosenthal <mrosenthal@ucvts.org>
2019-02-27 12:36:29 -0500

    shape, line, css, canvas size

--- js/draw.js
+++ js/draw.js
@@ -1,40 +1,41 @@
 let firstClick = true;
-// let canvas = document.getElementById("canvas1").getContext("2d");
 let toolCurrent = "line";
 
 function toolType(tool) {
     toolCurrent = tool;
-    alert("runs function");
 }
 
 function draw() {
     let xCord = event.offsetX;
     let yCord = event.offsetY;
-    let canvas = document.getElementById("canvas1").getContext("2d");
-    if (firstClick && toolCurrent !== "fill"){
-            canvas.beginPath();
-            canvas.moveTo(xCord, yCord);
+    let ctx = document.getElementById("canvas1").getContext("2d");
+    if (firstClick /* && toolCurrent !== "bucket" */){
+            ctx.beginPath();
+            ctx.moveTo(xCord, yCord);
             firstClick = false;
     }
 
-    if (toolCurrent === "line"){
-      canvas.lineTo(xCord, yCord);
-    // } else if (toolCurrent === "poly") {
-    // } else if (toolCurrent === "fill"){
-    //   canvas.fill();
+    if (toolCurrent === "shape" || toolCurrent === "line"){
+      ctx.lineTo(xCord, yCord);
+      ctx.stroke();
     }
+    // } else if (toolCurrent === "poly") {
+    // } else if (toolCurrent === "bucket"){
+    //   canvas.fill();
 }
 
-function endLine() {
-    let canvas = document.getElementById("canvas1").getContext("2d");
-    canvas.closePath();
-    canvas.fillStyle = "black";
-    canvas.fill();
+function endShape() {
+    let ctx = document.getElementById("canvas1").getContext("2d");
+    ctx.closePath();
+    if (toolCurrent === "shape"){
+      ctx.fillStyle = "black";
+      ctx.fill();
+    }
     firstClick = true;
 }
 
 function clear() {
-  let canvas = document.getElementById("canvas1").getContext("2d");
-  canvas.clearRect(0, 0, 1500, 1500);
+  let ctx = document.getElementById("canvas1").getContext("2d");
+  ctx.clearRect(0, 0, 1500, 1500);
   alert("happens");
 }
