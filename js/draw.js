0434ad25691223a99bd9e28172034b7e7fa7792f
Meredith Rosenthal <mrosenthal@ucvts.org>
2019-02-27 13:23:44 -0500

    starting polyline

--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,8 @@
 let firstClick = true;
 let toolCurrent = "line";
+let xPoints = [];
+let yPoints = [];
+// let i = 0;
 
 function toolType(tool) {
     toolCurrent = tool;
@@ -8,6 +11,10 @@ function toolType(tool) {
 function draw() {
     let xCord = event.offsetX;
     let yCord = event.offsetY;
+    // xPoints.push(xCord);
+    // yPoints.push(yCord);
+    alert(curvePoints[0]);
+    // i++;
     let ctx = document.getElementById("canvas1").getContext("2d");
     if (firstClick /* && toolCurrent !== "bucket" */){
             ctx.beginPath();
@@ -19,8 +26,9 @@ function draw() {
       ctx.lineTo(xCord, yCord);
       ctx.strokeStyle = colorDropdown();
       ctx.stroke();
+    } else if (toolCurrent === "poly") {
+      // polyline(curvePoints[0], curvePoints[1], curvePoints[2], curvePoints[3]);
     }
-    // } else if (toolCurrent === "poly") {
     // } else if (toolCurrent === "bucket"){
     //   canvas.fill();
 }
@@ -45,3 +53,11 @@ function colorDropdown(){
   let a = document.getElementById("color");
   return a.value;
 }
+
+// function polyline(start, cp1, cp2, end){
+//   let ctx = document.getElementById("canvas1").getContext("2d");
+//   ctx.beginPath();
+//   ctx.moveTo(start.x, start.y);
+//   ctx.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, end.x, end.y);
+//   ctx.stroke();
+// }
